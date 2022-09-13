a36209fb3713bc6a74c69efc0f77fb82ddbb6057
Guilherme Torres <guilherme.torres1@creditas.com>
2022-09-12 23:50:28 -0300

    feat: setup mobile/web

---
+++ mobile/App.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Button title="Send 1"></Button>
+    </View>
+  );
+}
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text style={styles.button}>{props.title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    fontSize: 22,
+  },
+});
---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+interface ButtonProps {
+  title: string;
+}
+function Button(props: ButtonProps) {
+  return <button>{props.title}</button>;
+}
+
+function App() {
+  return (
+    <>
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+    </>
+  );
+}
+
+export default App;
