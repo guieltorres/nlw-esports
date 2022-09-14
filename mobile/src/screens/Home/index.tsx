13db29b51b3dd960cc02aa83194a1354b1b8c6a7
Guilherme Torres <guilherme.torres1@creditas.com>
2022-09-14 13:45:14 -0300

    feat: added web/mobile template

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import { View, Image, FlatList } from "react-native";
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
+import { GameCard } from "../../components/GameCard";
+import { GAMES } from "../../utils/games";
+
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={logoImg} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o jogo que deseja jogar..."
+      />
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.contentList}
+      />
+    </View>
+  );
+}
